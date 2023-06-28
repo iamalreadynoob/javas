0c222c0267f7d856d944eb8e9f4cd3d63e6e1c07
iamalreadynoob <22120205381@ismu.edu.tr>
2023-06-28 15:11:13 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,6 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args)
     {
-        ArrayList<String> test = new ArrayList<>();
-        test.add("2.3");
-        test.add("17.8");
-        test.add("Orlando Bloom");
-        test.add("89");
-        test.add("347");
-        test.add("999987");
-        test.add("23.3");
 
-        ArrayList<String> requested = ListSimplify.getRequestedItems(test, null, ListSimplify.Subprocess.NOT_FLOATING);
-        System.out.println(requested);
     }
 }
